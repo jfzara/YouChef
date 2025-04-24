ad330a533c6dc4abc7ec2cf1b95321c88981b9ba
jfzara <zarajeanfabrice@gmail.com>
2025-04-24 18:58:16 -0400

    ajout des liens pour les boutons de l accueil

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import styles from '../styles/Accueil.module.css';
+import React from "react";
+import styles from "../styles/Accueil.module.css";
+import { Link } from "react-router-dom";
 
 const Accueil = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.titre}>Bienvenue sur notre application de recettes !</h1>
-      <p className={styles.sousTitre}>Trouvez, partagez et sauvegardez vos recettes préférées 🍲</p>
-      
+      <h1 className={styles.titre}>
+        Bienvenue sur notre application de recettes !
+      </h1>
+      <p className={styles.sousTitre}>
+        Trouvez, partagez et sauvegardez vos recettes préférées 🍲
+      </p>
+
       <div className={styles.ctaSection}>
-        <button className={styles.bouton}>Commencer</button>
-        <button className={styles.boutonSecondaire}>Parcourir les recettes</button>
+        <Link to="/inscription">
+          <button className={styles.bouton}>Commencer</button>
+        </Link>
+        <Link to="/inscription">
+          <button className={styles.boutonSecondaire}>
+            Parcourir les recettes
+          </button>
+        </Link>
       </div>
 
       <section className={styles.extrait}>
